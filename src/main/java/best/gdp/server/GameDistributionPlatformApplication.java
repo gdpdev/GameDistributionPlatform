9d0e369451672892132cbe036c389157353ea392
gdpdev <service@gdp.best>
2019-09-04 17:25:49 +0800

    first commit

---
+++ src/main/java/best/gdp/server/GameDistributionPlatformApplication.java
@@ -0,0 +1,13 @@
+package best.gdp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDistributionPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameDistributionPlatformApplication.class, args);
+    }
+
+}
